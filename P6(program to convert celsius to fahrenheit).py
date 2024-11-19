3b147f2623fca8fc4f74106a0ea67558c0f6ef06
MUDIT SINGH <157034450+whomuditsingh@users.noreply.github.com>
2024-11-19 20:54:31 +0530

    Add files via upload

---
+++ P6(program to convert celsius to fahrenheit).py
@@ -0,0 +1,5 @@
+#program to convert celsius to fahrenheit
+celsius=float(input("enter a number:\n"))
+#conversion formula:fahrenheit=(celsius*9/5)+32
+fahrenheit=(celsius*9/5)+32
+print(f"{celsius} degree celsius is equal to {fahrenheit} degrees fahrenheit")
