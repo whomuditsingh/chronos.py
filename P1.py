9649376be48aff02ffa33ffd37cc9f74cdd7a8d6
MUDIT SINGH <157034450+whomuditsingh@users.noreply.github.com>
2024-11-19 18:43:22 +0530

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,16 @@
+"""print("hello bharat")
+print("lets do mathematics operation:")
+#ADDITION
+a=float(input("enter first number:"))
+b=float(input("enter second number:"))
+sum= a+b
+print(sum)"""
+
+#divison
+a=float(input("enter first number:"))
+b=float(input("enter second number:"))
+if b==0:
+    print("denominator can not be equals to Zero")
+else: 
+    Division=(a/b)
+    print(Division)
---
+++ P2 (FIND AREA OF A TRAINGLE).py
@@ -0,0 +1,7 @@
+#FIND AREA OF A TRAINGLE
+#AREA OF TRIAGLE=1/2(BASE*HEIGHT OF TRAINGLE)
+base=float(input("base of traingle:"))
+height=float(input("height of traingle:"))
+area=0.5*base*height
+print("area of triangle is:")
+print(area)
